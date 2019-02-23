b4e5355cbca5126384c444079b800d4819269543
samnejati <37846419+samnejati@users.noreply.github.com>
2019-02-23 14:25:36 -0700

    Add files via upload

---
+++ Calculator.java
@@ -0,0 +1,54 @@
+
+/*
+
+
+@author Hesssam Nejati
+Programmed by: Hessam Nejati
+
+
+
+
+ */
+
+//
+//b. Copy the file Calculator.java to this package. Document this file using the Javadoc comment style but do not
+// make any code changes to the file at this time. Most of the methods are blank.
+//		c. Commit the project to git and github (or bitbucket)
+
+
+
+
+
+
+public class Calculator {
+
+	private int total;
+	
+	public Calculator () {
+		total = 0;  // not needed - included for clarity
+	}
+	
+	public int getTotal () {
+		return 0;
+	}
+	
+	public void add (int value) {
+		
+	}
+	
+	public void subtract (int value) {
+		
+	}
+	
+	public void multiply (int value) {
+		
+	}
+	
+	public void divide (int value) {
+		
+	}
+	
+	public String getHistory () {
+		return "";
+	}
+}
