e4106333d44a6176f1a331f0b727eaac2551b2b9
samnejati <37846419+samnejati@users.noreply.github.com>
2019-02-23 14:49:23 -0700

    Calculator program Version 2.0

--- Calculator.java
+++ Calculator.java
@@ -4,36 +4,47 @@
 *This calculator simulator
 *
 * @author Hesssam Nejati
-* @version 1.0
+* @version 2.0
 * @since 2019-02-23
 */
+
 
 public class Calculator {
 
 	private int total;
 	
 	public Calculator () {
-		total = 0;  // not needed - included for clarity
+		total = 0;
 	}
-	
+
+	//The getTotal method should return the current total
 	public int getTotal () {
-		return 0;
+		return total;
 	}
-	
+
+	//The add method adds the parameter to the total variable
 	public void add (int value) {
-		
+		total = total + value;
 	}
-	
+
+	//The subtract method subtracts the parameter from the total variable
 	public void subtract (int value) {
-		
+		total = total - value;
 	}
-	
+
+	// The multiply method multiplies the total by the parameter
 	public void multiply (int value) {
-		
+		total = total * value;
 	}
-	
+
+	//The divide method divides the total by the parameter. Use integer division.
+	// If the parameter is zero, set the total to zero. Do not print an error message or rasise an exception..
 	public void divide (int value) {
-		
+		if (value == 0){
+			total = 0;
+		} else{
+			total = total / value;
+		}
 	}
 	
 	public String getHistory () {
