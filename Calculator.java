677684cbe097cdbb71ecc727efb78a270589c3de
samnejati <37846419+samnejati@users.noreply.github.com>
2019-02-23 14:33:53 -0700

    Version 1.0 Calculator Program

---
+++ Calculator.java
@@ -0,0 +1,42 @@
+
+/**
+* This program contains a framework for a calulator. In this version 1 all the methods are avilable but all are non functioning.
+*This calculator simulator
+*
+* @author Hesssam Nejati
+* @version 1.0
+* @since 2019-02-23
+*/
+
+public class Calculator {
+
+	private int total;
+	
+	public Calculator () {
+		total = 0;  // not needed - included for clarity
+	}
+	
+	public int getTotal () {
+		return 0;
+	}
+	
+	public void add (int value) {
+		
+	}
+	
+	public void subtract (int value) {
+		
+	}
+	
+	public void multiply (int value) {
+		
+	}
+	
+	public void divide (int value) {
+		
+	}
+	
+	public String getHistory () {
+		return "";
+	}
+}
